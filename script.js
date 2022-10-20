8269fb3f43b5a9c6e3db78654a2247f43ec0b478
Wojtek <bober.developer@gmail.com>
2022-10-20 20:42:54 +0200

    add play and pause button

--- script.js
+++ script.js
@@ -7,4 +7,35 @@ const stopwatch = document.querySelector('.stopwatch')
 const time = document.querySelector('.time')
 const timeList = document.querySelector('.time-list')
 
+const infoBtn = document.querySelector('.info')
+const modalShadow = document.querySelector('.modal-shadow')
+const closeModalBtn = document.querySelector('.close')
 
+let countTime
+let minutes = 0
+let seconds = 0
+
+const handleStart = () => {
+	clearInterval(countTime)
+
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++
+			stopwatch.textContent = `${minutes}:0${seconds}`
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++
+			stopwatch.textContent = `${minutes}:${seconds}`
+		} else {
+			minutes++
+			seconds = 0
+			stopwatch.textContent = `${minutes}:00`
+		}
+	}, 50)
+}
+
+const handlePause = () => {
+    clearInterval(countTime)
+}
+
+startBtn.addEventListener('click', handleStart)
+pauseBtn.addEventListener('click', handlePause)
