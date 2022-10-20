0dadb9f9d9d9113a555011fb82931a8a82079526
Wojtek <bober.developer@gmail.com>
2022-10-20 20:15:26 +0200

    add html and css, then download all elements

---
+++ script.js
@@ -0,0 +1,10 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const resetBtn = document.querySelector('.reset')
+const archivBtn = document.querySelector('.archiv')
+const stopwatch = document.querySelector('.stopwatch')
+const time = document.querySelector('.time')
+const timeList = document.querySelector('.time-list')
+
+
